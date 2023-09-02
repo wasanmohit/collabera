f5c6ec3275cfdef17079b0d0676919b00b5cb916
wasanmohit <wasan.mohit@gmail.com>
2023-09-02 13:47:00 +0530

    added kube file

---
+++ kube/kube.tf
@@ -0,0 +1,48 @@
+#using the kubernetes provider
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.48.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "/root/eks-cluster/terraform.tfstate"
+  }
+}
+
+# Retrieve EKS cluster information
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = "terraformEKScluster"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.eks_cluster.name
+    ]
+  }
+}
+
